ebeff410cd2e8718c9b71729ed1b3c92db56deeb
Brimstone <brimstonee12@gmail.com>
2019-09-30 17:21:39 +0200

    commit

--- Server/server.js
+++ Server/server.js
@@ -10,7 +10,7 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 
-app.use(express.static(path.join(__dirname,'client/public/')))
+app.use(express.static(path.join(__dirname,'client/build')))
 
 
 //CONNECT TO DB
