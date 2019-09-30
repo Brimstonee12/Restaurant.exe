a570cf120e8d6c0f4f149a57f98aca18f5e37078
Brimstone <brimstonee12@gmail.com>
2019-09-30 18:21:34 +0200

    commit

--- Server/server.js
+++ Server/server.js
@@ -15,10 +15,11 @@ app.use(express.static(path.join(__dirname,'client/build')))
 
 if(process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, 'client/build')));
-    app.get('*', (req, res) => {    res.sendfile(path.join(__dirname = 'client/build/index.html'));
+    app.get('*', (req, res) => {res.sendfile(path.join(__dirname = 'client/build/index.html'));
       })}
 
-app.get('*', (req, res) => {  res.sendFile(path.join(__dirname+'/client/public/index.html'));})
+// app.get('*', (req, res) => {  res.sendFile(path.join(__dirname+'/client/public/index.html'))})
+
 //CONNECT TO DB
 mongoose.connect('mongodb+srv://Brimstonee12:huaweip29l@respository-mmcvv.mongodb.net/test?retryWrites=true&w=majority',
  {useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true  });
