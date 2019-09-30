0c37d5915e764a42f0ffc40ca2bddaf125d3f83c
Brimstone <brimstonee12@gmail.com>
2019-09-30 18:08:16 +0200

    commit

--- Server/server.js
+++ Server/server.js
@@ -10,9 +10,15 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 
+
 app.use(express.static(path.join(__dirname,'client/build')))
 
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, 'client/build')));
+    app.get('*', (req, res) => {    res.sendfile(path.join(__dirname = 'client/build/index.html'));
+      })}
 
+app.get('*', (req, res) => {  res.sendFile(path.join(__dirname+'/client/public/index.html'));})
 //CONNECT TO DB
 mongoose.connect('mongodb+srv://Brimstonee12:huaweip29l@respository-mmcvv.mongodb.net/test?retryWrites=true&w=majority',
  {useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true  });
