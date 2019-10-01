33b54eeb1d007fdfa9b8790628cf66d27ced1c8b
Brimstone <brimstonee12@gmail.com>
2019-10-01 14:54:00 +0200

    commit

--- Server/server.js
+++ Server/server.js
@@ -25,13 +25,10 @@ connection.once('open', () => {
 app.use('/reserved', ReservedRouter);
 app.use('/tablesav', TablesAVRouter);
 
-
 if (process.env.NODE_ENV === 'production') {
-  //SET STATIC FOLDER
-  app.use(express.static('../client/build'));
-
+  app.use(express.static('client/build'));
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../','client', 'build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }
 
